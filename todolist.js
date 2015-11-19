8ef51b3eb9eb0565d354bfe2011708e880d808f3
Johall <johnalexander.w.hall@gmail.com>
2015-11-18 21:32:15 -0500

    created delete btn, done btn, and added mmore basic style elements

---
+++ todolist.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+
+ $(".alert-danger").hide();
+
+ $(".btn-default").on("click", function(e) {
+  e.preventDefault();
+
+  var newItem = $("#newItem").val().trim();
+  var isDuplicate = false;
+  $("td.word-td").each(function() {
+   if ($(this).text().trim().toLowerCase() === newItem.toLowerCase()) {
+    isDuplicate = true;
+    return;
+   }
+  });
+  if(isDuplicate) {
+   $(".alert-danger").slideDown();
+   return;
+  }
+
+  var newRow = $("<tr>");
+  var wordTd = $("<td>").addClass("word-td").append(newItem);
+  var deleteBtn = $("<button>").addClass("btn btn-danger").append("Remove");
+  var doneBtn = $("<button>").addClass("btn btn-success").append("Done");
+  var deleteTd = $("<td>").append(deleteBtn);
+  var doneTd = $("<td>").append(doneBtn);
+
+  newRow.append(wordTd).append(deleteTd).append(doneBtn);
+  $("tbody").append(newRow);
+
+  $("#newItem").val("").focus();
+
+ });
+
+ $("table").on("click", ".btn-danger", function() {
+  if($("tr").length > 2) {
+   $(this).parent().parent().remove();
+  }
+
+ });
+
+
+ $("#newItem").on("keydown", function () {
+  $(".alert-danger").fadeOut(1500);
+ });
+
+
+});
+
+
+
+
+
+
+
+
+
+
+ 
